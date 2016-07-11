e3c28899f8c9bdc14ccfb1c24f3b7546ebd88c0b
Gene Chuang <gc.genechuang@gmail.com>
2016-07-11 17:51:54 -0400

    First Upload
    Source Code

---
+++ src/main/java/GeneGroupId1/GeneMavenTestProj1/App.java
@@ -0,0 +1,13 @@
+package GeneGroupId1.GeneMavenTestProj1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
